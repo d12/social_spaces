8a9fceba929704aa386df69a143e00aec1f77a74
Nathaniel Woodthorpe <nathaniel.woodthorpe@shopify.com>
2020-06-10 19:40:17 -0400

    Formatting

--- app/models/group.rb
+++ app/models/group.rb
@@ -9,6 +9,7 @@ class Group < ApplicationRecord
 
   def generate_key_if_missing
     return if self.key
+
     # Generates a random string from a set of easily readable characters
     charset = %w{ 2 3 4 6 7 9 A C D E F G H J K M N P Q R T V W X Y Z}
     self.key = 6.times.map { charset.to_a[rand(charset.size)] }.join
