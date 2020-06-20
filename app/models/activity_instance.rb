f34ad47ea3b8999b04db904b3697d27adfb60fdb
Nathaniel Woodthorpe <nathaniel.woodthorpe@shopify.com>
2020-06-20 19:43:21 -0400

    Make the app more simple by removing Activity:: in favor of ActivityInstance + STI

--- app/models/activity_instance.rb
+++ app/models/activity_instance.rb
@@ -1,24 +1,38 @@
 class ActivityInstance < ApplicationRecord
+  self.inheritance_column = "activity"
+
   belongs_to :group
   enum status: { awaiting_activity_thread: 0, ongoing: 1, finished: 2 }
 
   before_create :initialize_storage
 
-  def send_message(data)
-    activity_klass.message(self.reload, data)
+  def self.display_name
+    "Twenty Questions"
+  end
+
+  def self.max_users
+    8
+  end
+
+  def message(data)
+    raise NotImplementedError
   end
 
   def client_bootstrap_data
-    activity_klass.client_bootstrap_data(self.reload)
+    raise NotImplementedError
+  end
+
+  def tick
+    raise NotImplementedError
   end
 
   private
 
   def initialize_storage
-    self.state ||= activity_klass.initialize_storage
+    self.state ||= initial_state
   end
 
-  def activity_klass
-    "Activity::#{activity.camelize}".constantize
+  def initial_state
+    raise NotImplementedError
   end
 end
---
+++ app/models/twenty_questions.rb
@@ -0,0 +1,35 @@
+class TwentyQuestions < ActivityInstance
+  def self.display_name
+    "Twenty Questions"
+  end
+
+  def self.max_users
+    8
+  end
+
+  def message(data)
+    return unless data["add"]
+
+    state["count"] += 1
+    save!
+
+    {"updatedCount" => state["count"]}
+  end
+
+  def tick
+
+  end
+
+  # All the data required for a client to bootstrap itself
+  # E.g. When a client joins midway, they need enough information
+  # to render the current state of the game
+  def client_bootstrap_data
+    reload
+    {"count" => state["count"]}
+  end
+
+  # The initial value to use for a instances save state
+  def initial_state
+    {"count" => 0}
+  end
+end
