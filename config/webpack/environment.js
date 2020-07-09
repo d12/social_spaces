4ced0bbb9bce2d5dda89eccc48d21d41b62a95d8
Nathaniel Woodthorpe <nathaniel.woodthorpe@shopify.com>
2020-07-09 15:10:49 -0400

    Fix slow webpacker problems

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,5 +1,7 @@
-const { environment } = require('@rails/webpacker')
-const typescript =  require('./loaders/typescript')
+const { environment } = require("@rails/webpacker");
+const typescript = require("./loaders/typescript");
 
-environment.loaders.prepend('typescript', typescript)
-module.exports = environment
+environment.loaders.prepend("typescript", typescript);
+environment.loaders.delete("nodeModules");
+
+module.exports = environment;
