3e5f21eac09a5ca60dd5fa67c4a61dc0537b8508
Nathaniel Woodthorpe <nathaniel.woodthorpe@shopify.com>
2020-11-15 22:38:14 -0500

    Put a video on the right side of the UI

--- app/javascript/components/AppFrame/AppFrame.tsx
+++ app/javascript/components/AppFrame/AppFrame.tsx
@@ -16,11 +16,14 @@ import {
   Grid,
   Box,
   TextField,
+  Paper,
 } from "@material-ui/core";
 import { ThemeProvider, makeStyles } from "@material-ui/core/styles";
 import { theme } from "theme";
 import Toast, { ToastSeverity } from "./Toast";
 import consumer from "../../channels/consumer";
+
+declare var JitsiMeetExternalAPI: any;
 
 export interface Props {
   children?: React.ReactNode;
@@ -48,21 +51,19 @@ const useStyles = makeStyles((_theme) => ({
     flexGrow: 1,
   },
   appBar: {
-    width: `calc(100% - ${drawerWidth}px)`,
+    flexGrow: 1,
     marginLeft: drawerWidth,
   },
   mainBody: {
     flexGrow: 1,
   },
-  drawer: {
-    width: drawerWidth,
-    flexShrink: 0,
-  },
   drawerPaper: {
     width: drawerWidth,
+    height: "100%",
   },
   leaveGroupButton: {
     width: buttonWidth,
+    marginTop: "auto"
   },
   leaveGroupLink: {
     width: buttonWidth,
@@ -71,8 +72,21 @@ const useStyles = makeStyles((_theme) => ({
     marginTop: "auto",
   },
   logoutWithDrawer: {
-    marginRight: drawerWidth,
   },
+  container: {
+    flexWrap: "nowrap",
+  },
+  drawerFlexBox: {
+    height: "100%",
+    flexDirection: "column"
+  },
+  foo: {
+    width: "800px",
+  },
+  headerBox: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  }
 }));
 
 export function AppFrame({
@@ -111,69 +125,107 @@ export function AppFrame({
           },
         }
       );
+
+      const domain = '8x8.vc';
+      const options = {
+          roomName: 'vpaas-magic-cookie-cb5f846d50d54f4eb3ecfbdfc3875b94/Testing',
+          interfaceConfigOverwrite: {
+            TILE_VIEW_MAX_COLUMNS: 1,
+            DISPLAY_WELCOME_FOOTER: false,
+            DISPLAY_WELCOME_PAGE_CONTENT: false,
+            DISPLAY_WELCOME_PAGE_ADDITIONAL_CARD: false,
+            DISPLAY_WELCOME_PAGE_TOOLBAR_ADDITIONAL_CONTENT: false,
+            DISABLE_VIDEO_BACKGROUND: true,
+            GENERATE_ROOMNAMES_ON_WELCOME_PAGE: false,
+            HIDE_INVITE_MORE_HEADER: true,
+            MOBILE_APP_PROMO: false,
+            RECENT_LIST_ENABLED: false,
+            SHOW_CHROME_EXTENSION_BANNER: false,
+            SHOW_JITSI_WATERMARK: false,
+            SHOW_POWERED_BY: false,
+            SHOW_PROMOTIONAL_CLOSE_PAGE: false,
+            TOOLBAR_ALWAYS_VISIBLE: false,
+            TOOLBAR_BUTTONS: [
+              'microphone',
+              'camera',
+              'desktop',
+              'fullscreen',
+              'fodeviceselection',
+              'chat',
+              'settings',
+              'videoquality',
+              'tileview',
+            ],
+            VERTICAL_FILMSTRIP: true,
+            VIDEO_QUALITY_LABEL_DISABLED: true
+          },
+          jwt: "eyJhbGciOiJSUzI1NiIsImtpZCI6InZwYWFzLW1hZ2ljLWNvb2tpZS1jYjVmODQ2ZDUwZDU0ZjRlYjNlY2ZiZGZjMzg3NWI5NC9kMjUwZmMiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJqaXRzaSIsImNvbnRleHQiOnsidXNlciI6eyJpZCI6IjEiLCJuYW1lIjoiTmF0aGFuaWVsIFdvb2R0aG9ycGUiLCJhdmF0YXIiOiJodHRwczovL2F2YXRhcnMwLmdpdGh1YnVzZXJjb250ZW50LmNvbS91LzMwOTIwMjE2IiwiZW1haWwiOiJuandvb2R0aG9ycGVAZ21haWwuY29tIiwibW9kZXJhdG9yIjoidHJ1ZSJ9LCJmZWF0dXJlcyI6eyJsaXZlc3RyZWFtaW5nIjoidHJ1ZSIsInJlY29yZGluZyI6ImZhbHNlIn19LCJleHAiOjE2MDU0OTk3ODIsImlzcyI6ImNoYXQiLCJuYmYiOjE2MDU0OTYwODIsInJvb20iOiIqIiwic3ViIjoidnBhYXMtbWFnaWMtY29va2llLWNiNWY4NDZkNTBkNTRmNGViM2VjZmJkZmMzODc1Yjk0In0.Pf_R2dJSU5MLb0lQ2jtoYxqLQJO9mwC6p6bhvTzO5Azvu_Dw9DBNeah0VuROAhu3SvJH40YAIyClFnaKfytrQlc91mZLK5sPDSlLXpW6--uWfXVn7k7dvYzRENa82YtU6N61O6s3XXDYfhVVaurrmPT1D8_hdJilcsGbOPjqKrDpzrgAmyR_6kTDgJxEwumyTNa6D8MSrJoPgGFWa1n97u3vy_OQg8QpdYwLqNUPWnh-i2VhZlrx0VX-JwGDqgaxDvw9_-eAMArXTcID5WPCy3x9Q8uYX5IAZgWgYTzMHUU6ZJ3kkIkfY7NLwQ6S2ZvHKOQsIQtudhq5isoREQiMF3289-phTsNlBVmh1zfMfh_ogi8di21GmAnWbBHdSar-SWhQ69ANkaADhMmY4uT1m9995Z6FIyrNZzeEODETT1m5oLPFoZh3TF-TSYf6cOZXhfGqGLvoT89oVKvxIHtKQDAS1-lGljr38woi7R4I8tJeC9g7IKNoK7y0Dd9RhB6ABdnGVnO-NLdT9MZOeCdEKVPNKcJuFTjJpxvLFx4ibynkIdBrYfdalrZEpah08bX8MMCwkh9MbfaLCeTQOj8185ofFyJ5MQdPuYPpu-WrFJauRsV4LSFFvkm7TR-vjMgnR-XddOExCOUkPHCw2AsDfr6_Ht0cUul9omTikV8Sydg",
+          parentNode: document.querySelector('#video-container')
+      };
+
+      const api = new JitsiMeetExternalAPI(domain, options);
   }, []);
 
   const classes = useStyles();
 
   const groupBarMarkup = groupTabProps && (
     <Grid item>
-      <Drawer
-        className={classes.drawer}
-        variant="permanent"
-        classes={{
-          paper: classes.drawerPaper,
-        }}
-        anchor="right"
+      <Paper
+        className={classes.drawerPaper}
       >
-        <Box p={2}>
-          <Typography variant="h5">My Group</Typography>
-          <TextField
-            label="Google Meet URL"
-            InputProps={{
-              readOnly: true,
-            }}
-            value={groupTabProps.meetUrl}
-            fullWidth
-            margin="dense"
-          />
-          <TextField
-            label="Group Code"
-            InputProps={{
-              readOnly: true,
-            }}
-            value={groupTabProps.groupId}
-            fullWidth
-            margin="dense"
-          />
-        </Box>
-        <Divider variant="middle" />
-        <List>
-          {groupTabProps.users.map(({ email, name, gravatarUrl }) => (
-            <ListItem alignItems="center" key={email}>
-              <ListItemAvatar>
-                <Avatar alt={name} src={gravatarUrl} />
-              </ListItemAvatar>
-              <ListItemText primary={name} />
-            </ListItem>
-          ))}
-        </List>
-        <Link
-          rel="nofollow"
-          data-method="delete"
-          href="/leave_group"
-          underline="none"
-          color="textPrimary"
-          className={classes.leaveGroupLink}
+        <Grid
+          container
+          className={classes.drawerFlexBox}
         >
-          <Button
-            variant="outlined"
-            color="secondary"
-            className={classes.leaveGroupButton}
+          <Box p={2}>
+            <Typography variant="h5">My Group</Typography>
+            <TextField
+              label="Google Meet URL"
+              InputProps={{
+                readOnly: true,
+              }}
+              value={groupTabProps.meetUrl}
+              fullWidth
+              margin="dense"
+            />
+            <TextField
+              label="Group Code"
+              InputProps={{
+                readOnly: true,
+              }}
+              value={groupTabProps.groupId}
+              fullWidth
+              margin="dense"
+            />
+          </Box>
+          <List>
+            {groupTabProps.users.map(({ email, name, gravatarUrl }) => (
+              <ListItem alignItems="center" key={email}>
+                <ListItemAvatar>
+                  <Avatar alt={name} src={gravatarUrl} />
+                </ListItemAvatar>
+                <ListItemText primary={name} />
+              </ListItem>
+            ))}
+          </List>
+          <Link
+            rel="nofollow"
+            data-method="delete"
+            href="/leave_group"
+            underline="none"
+            color="textPrimary"
+            className={classes.leaveGroupLink}
           >
-            Leave Group
-          </Button>
-        </Link>
-      </Drawer>
+            <Button
+              variant="outlined"
+              color="secondary"
+              className={classes.leaveGroupButton}
+            >
+              Leave Group
+            </Button>
+          </Link>
+        </Grid>
+      </Paper>
     </Grid>
   );
 
@@ -195,13 +247,21 @@ export function AppFrame({
     <Toast message={noticeToast} severity={ToastSeverity.INFO} />
   );
 
+
+
   return (
     <ThemeProvider theme={theme}>
       {alertMarkup || noticeMarkup}
-      <Grid container>
+      <Grid
+       container
+       direction="row"
+       justify="flex-start"
+       align-items="flex-start"
+       className={classes.container}
+      >
         <Grid item className={classes.mainBody}>
-          <AppBar color="transparent" variant="outlined">
-            <Toolbar variant="dense">
+          <AppBar color="transparent" variant="outlined" position="relative">
+            <Toolbar variant="dense" className={classes.headerBox}>
               <Typography variant="h6" className={classes.appTitle}>
                 Social Spaces
               </Typography>
@@ -217,10 +277,11 @@ export function AppFrame({
               </Link>
             </Toolbar>
           </AppBar>
-          <Toolbar />
           <Container maxWidth="lg">{children}</Container>
         </Grid>
         {groupBarMarkup}
+        <Box className={classes.foo} id="video-container">
+        </Box>
       </Grid>
     </ThemeProvider>
   );
