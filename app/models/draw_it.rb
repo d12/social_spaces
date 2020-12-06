dc9cdfd930ae7d6083453ed5a49e3331894fe1ef
Nathaniel Woodthorpe <nathaniel.woodthorpe@shopify.com>
2020-12-06 16:45:07 -0500

    I can't spell

--- app/models/draw_it.rb
+++ app/models/draw_it.rb
@@ -2,7 +2,7 @@ class DrawIt < ActivityInstance
   register_event "draw", DrawIt::EventHandlers::Draw
   register_event "user_joined", DrawIt::EventHandlers::UserJoined
 
-  has_many :draw_events, class_name: "DrawIt::DrawEvent", foreign_key: "activity_instance_id", dependant: :destroy
+  has_many :draw_events, class_name: "DrawIt::DrawEvent", foreign_key: "activity_instance_id", dependent: :destroy
 
   class Status
     DRAWING = :drawing
