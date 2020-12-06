bc08ead9c70130b6df30a696171498359748d6f0
Nathaniel Woodthorpe <nathaniel.woodthorpe@shopify.com>
2020-12-06 16:20:40 -0500

    Remove constant from the routes file, rails is complaining

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
-ROOM_KEY_CONSTRAINT = /[a-zA-Z\d]{6}/
-
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   mount ActionCable.server => '/cable'
 
-  get "/(:room_key)", to: "react#show", constraints: { room_key: ROOM_KEY_CONSTRAINT }, as: "react"
+  get "/(:room_key)", to: "react#show", constraints: { room_key: /[a-zA-Z\d]{6}/ }, as: "react"
 
-  get "/login/google(/join/:room_key)", to: "sessions#redirect_to_google_oauth", constraints: { room_key: ROOM_KEY_CONSTRAINT }
+  get "/login/google(/join/:room_key)", to: "sessions#redirect_to_google_oauth", constraints: { room_key: /[a-zA-Z\d]{6}/ }
 
   delete "/logout", to: "sessions#destroy"
 
