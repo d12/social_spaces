16288e8f188aff368b26aeb8ff1360b80c4066b1
Nathaniel Woodthorpe <njwoodthorpe@gmail.com>
2021-01-18 15:55:18 -0500

    Update webpack config to support new version or something I think

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,7 +1,32 @@
 const { environment } = require("@rails/webpacker");
-const typescript = require("./loaders/typescript");
 
+const webpackMerge = require('webpack-merge')
+const path = require('path')
+const webpack = require('webpack')
+
+const typescript = require("./loaders/typescript");
 environment.loaders.prepend("typescript", typescript);
 environment.loaders.delete("nodeModules");
 
-module.exports = environment;
+const webpackerDefaultWebpackConfig = environment.toWebpackConfig().toObject()
+
+const mergeData = {
+  node: {
+    dgram: 'empty',
+    fs: 'empty',
+    net: 'empty',
+    tls: 'empty',
+    child_process: 'empty'
+  }
+}
+
+const mergeStrategies = {
+  'resolve.modules': 'replace',
+}
+
+const mergedWebpackConfig = webpackMerge.smartStrategy(mergeStrategies)(
+  webpackerDefaultWebpackConfig,
+  mergeData
+)
+
+module.exports = mergedWebpackConfig;
