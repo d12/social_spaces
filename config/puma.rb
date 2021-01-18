80567f57f681833a9acb64fb26ff53e04e0ccc8a
Nathaniel Woodthorpe <njwoodthorpe@gmail.com>
2021-01-18 11:16:52 -0500

    Update database config to work with RDS and EBS

--- config/puma.rb
+++ config/puma.rb
@@ -17,7 +17,8 @@ port        ENV.fetch("PORT") { 3000 }
 environment ENV.fetch("RAILS_ENV") { "development" }
 
 # Specifies the `pidfile` that Puma will use.
-pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+bind "unix:///var/run/puma/my_app.sock"
+pidfile "/var/run/puma/my_app.sock"
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked web server processes. If using threads and workers together
@@ -36,3 +37,4 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
