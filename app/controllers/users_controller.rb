be22bf38d04db91db18a6f294b63891b626d1008
Nathaniel Woodthorpe <njwoodthorpe+d12@gmail.com>
2021-01-20 13:47:37 -0500

    Auto-join group when following an invite link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,25 @@ class UsersController < ApplicationController
   def create_guest
     if current_user
       head 403 && return
-    elsif guest_name.present?
-      u = User.create!(name: guest_name, guest: true)
-      session[:guest_user_id] = u.id
-      redirect_to react_path
-    else
-      head 400
-      puts "Bad!"
+    elsif guest_name.empty?
+      head 400 && return
     end
+
+    user = User.create!(name: guest_name, guest: true)
+    session[:guest_user_id] = user.id
+
+    session[:join_group_after_auth] = group_key
+
+    redirect_to react_path
   end
 
   private
 
   def guest_name
     params["name"]
+  end
+
+  def group_key
+    params["groupKey"]
   end
 end
