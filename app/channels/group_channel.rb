7b4e1e96e719dd121aa7549a4824228d48a3cae7
Nathaniel Woodthorpe <njwoodthorpe+d12@gmail.com>
2021-01-29 11:20:33 -0500

    Make our connection management logic a little more robust

--- app/channels/group_channel.rb
+++ app/channels/group_channel.rb
@@ -20,9 +20,43 @@ class GroupChannel < ApplicationCable::Channel
     current_user.update(disconnected_at: nil)
     @group = Group.find_by(key: params[:group_id])
     stream_for @group
+
+    add_connection_for_user(current_user.id)
   end
 
   def unsubscribed
-    current_user.update(disconnected_at: Time.zone.now)
+    remove_connection_for_user(current_user.id)
+
+    if(no_remaining_connections_for_user?(current_user.id))
+      current_user.update(disconnected_at: Time.zone.now)
+    end
+  end
+
+  private
+
+  def add_connection_for_user(user_id)
+    Redis.current.incr(connection_count_redis_key(user_id))
+  end
+
+  def remove_connection_for_user(user_id)
+    connection_count = connection_count_for_user(user_id).to_i
+
+    if connection_count <= 1
+      Redis.current.del(connection_count_redis_key(user_id))
+    else
+      Redis.current.decr(connection_count_redis_key(user_id))
+    end
+  end
+
+  def no_remaining_connections_for_user?(user_id)
+    connection_count_for_user(user_id) == "0"
+  end
+
+  def connection_count_for_user(user_id)
+    Redis.current.get(connection_count_redis_key(user_id)) || "0"
+  end
+
+  def connection_count_redis_key(user_id)
+    "group_channel_connections_for_user_#{user_id}"
   end
 end
